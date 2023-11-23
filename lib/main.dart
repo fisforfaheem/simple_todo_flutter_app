bbba30a9a814ffcf6797b5841904517f913039cd
Faheem <fisforfaheem@gmail.com>
2023-11-23 12:38:23 +0500

    chore: simple UI design

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: const MyHomePage(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.black,
         ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> todoList = [];
+
+  void addTodo() {
+    setState(() {
+      todoList.add("Item ${todoList.length + 1} ");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Demo"),
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+                title: Text(todoList[index]),
+                subtitle: const Text('Description'),
+                trailing: Checkbox(value: false, onChanged: (bool? value) {}));
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addTodo,
+        child: const Icon(Icons.add),
       ),
     );
   }
